fc506f0749579c852eeb19f4a17e420ba4a5b7b8
Andrii <andrubesaga1994@gmail.com>
2021-01-19 14:28:44 +0200

    fix

--- 7-gce/config.py
+++ 7-gce/config.py
@@ -32,7 +32,7 @@ DATA_BACKEND = 'cloudsql'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'testprojectv5'
+PROJECT_ID = 'terraform-test-300919'
 
 # CloudSQL & SQLAlchemy configuration
 # Replace the following values the respective values of your Cloud SQL
@@ -43,7 +43,7 @@ CLOUDSQL_DATABASE = 'bookshelf'
 # Set this value to the Cloud SQL connection name, e.g.
 #   "project:region:cloudsql-instance".
 # You must also update the value in app.yaml.
-CLOUDSQL_CONNECTION_NAME = 'testprojectv5:us-central1:bookshelf-db'
+CLOUDSQL_CONNECTION_NAME = 'terraform-test-300919:us-central1:bookshelf-db3'
 
 # The CloudSQL proxy is used locally to connect to the cloudsql instance.
 # To start the proxy, use:
@@ -90,7 +90,7 @@ MONGO_URI = 'mongodb://user:password@host:27017/database'
 #
 # You can adjust the max content length and allow extensions settings to allow
 # larger or more varied file types if desired.
-CLOUD_STORAGE_BUCKET = 'bookshelf-bucketv5'
+CLOUD_STORAGE_BUCKET = 'bookshelf-bucketv6'
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
--- 7-gce/gce/startup-script.sh
+++ 7-gce/gce/startup-script.sh
@@ -21,33 +21,47 @@ PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/
 
 # Install logging monitor. The monitor will automatically pickup logs sent to
 # syslog.
-# [START logging]
-curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
-service google-fluentd restart &
+# Install monitoring
+curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
+sudo bash add-monitoring-agent-repo.sh
+sudo apt-get update
+sudo apt-get install stackdriver-agent -y
+sudo service stackdriver-agent start
+# Install logging
+curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+sudo bash add-logging-agent-repo.sh
+sudo apt-get update
+sudo apt-get install -y google-fluentd-catch-all-config
+sudo service google-fluentd start
+
 # [END logging]
 
 # Install dependencies from apt
 apt-get update
 apt-get install -yq \
-    git build-essential supervisor python python-dev python-pip libffi-dev \
+    git build-essential supervisor python3-dev python3-pip libffi-dev \
     libssl-dev
 
 # Create a pythonapp user. The application will run as this user.
 useradd -m -d /home/pythonapp pythonapp
 
 # pip from apt is out of date, so make it update itself and install virtualenv.
-pip install --upgrade pip virtualenv
+pip3 install --upgrade pip virtualenv
 
 # Get the source code from the Google Cloud Repository
 # git requires $HOME and it's not set during the startup script.
 export HOME=/root
 git config --global credential.helper gcloud.sh
-git clone git clone https://source.developers.google.com/p/testprojectv5/r/7-gce /opt/app
+git clone https://source.developers.google.com/p/testprojectv5/r/7-gce /opt/app
 
 # Install app dependencies
 virtualenv -p python3 /opt/app/7-gce/env
 source /opt/app/7-gce/env/bin/activate
 /opt/app/7-gce/env/bin/pip install -r /opt/app/7-gce/requirements.txt
+
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
+./cloud_sql_proxy -instances=testprojectv5:us-central1:bookshelf-db=tcp:3306 &
 
 # Make sure the pythonapp user owns the application code
 chown -R pythonapp:pythonapp /opt/app
@@ -68,6 +82,7 @@ environment=VIRTUAL_ENV="/opt/app/7-gce/env",PATH="/opt/app/7-gce/env/bin",\
 stdout_logfile=syslog
 stderr_logfile=syslog
 EOF
+
 
 supervisorctl reread
 supervisorctl update
